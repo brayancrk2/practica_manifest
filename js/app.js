486835409ddc1fdf11c98af219650ed9fe0a7832
brayancrk2 <brayanmep13@gmail.com>
2023-12-07 13:43:18 -0600

    Primer Commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //obtenemos la url del servidor
 var url=window.location.href;
 //definimos que nuestro sw.js se encuentra en el repositorio
-var ubicacionSw='/Ejemplo-PWA/sw.js';
+var ubicacionSw='Downloads/ejemplo_cache/sw.js';
 
 if ( navigator.serviceWorker ) {
 /*Para que nuestro proyecto siga funcionando en localhost 
--- sw.js
+++ sw.js
@@ -1,106 +1,64 @@
-const CACHE ='cache-1';
-const CACHE_DINAMICO ='dinamico-1';
-const CACHE_INMUTABLE ='inmutable-1';
+const CACHE_NAME = 'my-site-cache-v1';
+const DYNAMIC_CACHE_NAME = 'dynamic-cache-v1';
+const MAX_DYNAMIC_ITEMS = 5;
 
-//Indicamos que durante el proceso de intalación se 
-//carguen los archivos del cache estatico
-self.addEventListener('install', evento=>{
-    /*Promesa que crea el proceso de creación del espacio 
-    en cache y agrega los archivos necesarios para cargar nuestra
-    aplicación*/
-    const promesa =caches.open(CACHE)
-        .then(cache=>{
-            return cache.addAll([
-               // '/',
-                'index.html',
-                'css/bootstrap.min.css',
-                'css/londinium-theme.css',
-                'css/styles.css',
-                'css/icons.css',
-                'js/bootstrap.min.js',
-                'js/application.js',
-                'js/app.js',
-                'offline.html',
-
-            ]);
-        });
- 
-        //Separamos los archivos que no se modificarán en un espacio de cache inmutable
- 
-       
-            //Indicamos que la instalación espere hasta que las promesas se cumplan
-        evento.waitUntil(Promise.all([promesa, cacheInmutable]));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/css/bootstrap.min.css',
+          '/css/londinium-theme.css',
+          '/css/styles.css',
+          '/js/bootstrap.min.js',
+          '/js/application.js',
+          '/js/app.js'
+          // Asegúrate de incluir todas las imágenes necesarias aquí
+        ]);
+      })
+  );
 });
-//Indicamos que durante el proceso de activación se borren los
-//espacios de cache estatico de versiones pasadas
-self.addEventListener('activate', evento =>{
-    //antes de activar el sw, obten los nombres de los espacios de cache existentes
-    const respuesta=caches.keys().then(keys =>{
-        //verifica cada nombre de espacios de cache
-        keys.forEach(key =>{
-            //si el espacio no tiene el nombre actual del cache e incluye la palabra cache
-            if(key !== CACHE && key.includes('cache')){
-                //borralo, la condición de include cache evitará borrar el espacio dinamico o inmutable
-                return caches.delete(key);
-            }
-        });
-    });
-    evento.waitUntil(respuesta);
-});
-//Atrapamos las peticiones y las procesamos con alguna estrategia de cache
-self.addEventListener('fetch', evento =>{
 
-    
-    //Estrategia 2 CACHE WITH NETWORK FALLBACK
-    const respuesta=caches.match(evento.request)
-        .then(res=>{
-            //si el archivo existe en cache retornalo
-            if (res) return res;
-
-        
-            //Procesamos la respuesta a la petición localizada en la web
-            return fetch(evento.request)
-                .then(resWeb=>{//el archivo recuperado se almacena en resWeb
-                    //se abre nuestro cache
-                    caches.open(CACHE_DINAMICO)
-                        .then(cache=>{
-                            //se sube el archivo descargado de la web
-                            cache.put(evento.request,resWeb);
-                            //Mandamos llamar la limpieza al cargar un nuevo archivo
-                            //estamos indicando que se limpiará el cache dinamico y que 
-                            //solo debe haber 2 archivos
-                            limpiarCache(CACHE_DINAMICO,2);
-                        })
-                    //se retorna el archivo recuperado para visualizar la página
-                    return resWeb.clone();  
-                });
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => {
+          return name !== CACHE_NAME && name !== DYNAMIC_CACHE_NAME;
+        }).map(name => {
+          return caches.delete(name);
         })
-        .catch(err => {
-            //si ocurre un error, en nuestro caso no hay conexión
-            if(evento.request.headers.get('accept').includes('text/html')){
-                //si lo que se pide es un archivo html muestra nuestra página offline que esta en cache
-                return caches.match('/offline.html');
-            }
-        });
-        evento.respondWith(respuesta);
-        
-
+      );
+    })
+  );
 });
 
-//recibimos el nombre del espacio de c ache a limpiar y el número de archivos permitido
-function limpiarCache(nombreCache, numeroItems){
-    //abrimos el cache
-    caches.open(nombreCache)
-        .then(cache=>{
-            //recuperamos el arreglo de archivos exixtentes en el espacio de cache
-            return cache.keys()
-                .then(keys=>{
-                    //si el número de archivos supera el limite permitido
-                    if (keys.length>numeroItems){
-                        //eliminamos el más antigüo y repetimos el proceso
-                        cache.delete(keys[0])
-                            .then(limpiarCache(nombreCache, numeroItems));
-                    }
-                });
+function limitCacheSize(cacheName, maxSize) {
+  caches.open(cacheName).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > maxSize) {
+        cache.delete(keys[0]).then(() => {
+          limitCacheSize(cacheName, maxSize);
         });
+      }
+    });
+  });
 }
+
+self.addEventListener('fetch', event => {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+            cache.put(event.request.url, fetchResponse.clone());
+            limitCacheSize(DYNAMIC_CACHE_NAME, MAX_DYNAMIC_ITEMS);
+            return fetchResponse;
+          });
+        });
+      })
+    );
+  }
+});
